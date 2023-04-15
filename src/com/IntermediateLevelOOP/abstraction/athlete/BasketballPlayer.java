4a16a55711969926a56e101c16e7af0837fe83ae
Daniel Baykov <danibaikov@abv.bg>
2023-04-15 15:21:04 +0300

    adding Abstraction - initial commit

---
+++ src/com/IntermediateLevelOOP/abstraction/athlete/BasketballPlayer.java
@@ -0,0 +1,45 @@
+package com.IntermediateLevelOOP.abstraction.athlete;
+
+import java.util.Random;
+
+public class BasketballPlayer extends Athlete {
+
+    private double freeThrowPercentage;
+    private double pointsPerGame;
+
+    public BasketballPlayer(String name, String nickname, int yearOfBirth, String team, double freeThrowPercentage, double pointsPerGame, int gamesPlayed) {
+        super(name, nickname, yearOfBirth, team, gamesPlayed);
+        this.freeThrowPercentage = freeThrowPercentage;
+        this.pointsPerGame = pointsPerGame;
+    }
+
+    public void freeThrow() {
+        Random randomNumberGenerator = new Random();
+        if (randomNumberGenerator.nextDouble(101) > freeThrowPercentage) {
+            System.out.println(super.getName() + " failed to score a free throw");
+        } else {
+            System.out.println(super.getName() + " scored a free throw");
+        }
+    }
+
+    @Override
+    public void getBio() {
+        super.getBio();
+        System.out.println("Free throw percentage: " + freeThrowPercentage);
+        System.out.println("Points per game: " + pointsPerGame);
+    }
+
+    @Override
+    public void playGame() {
+        super.playGame();
+        freeThrow();
+    }
+
+    public void setFreeThrowPercentage(double freeThrowPercentage) {
+        this.freeThrowPercentage = freeThrowPercentage;
+    }
+
+    public void setPointsPerGame(double pointsPerGame) {
+        this.pointsPerGame = pointsPerGame;
+    }
+}
