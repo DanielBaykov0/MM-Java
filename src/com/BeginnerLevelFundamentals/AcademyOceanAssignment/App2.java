df316f3ddf43afce492b6fda944f24f6dc51afae
Daniel Baykov <danibaikov@gmail.com>
2023-04-23 09:20:08 +0300

    adding Exercise 2 after suggested changes

--- src/com/BeginnerLevelFundamentals/AcademyOceanAssignment/App2.java
+++ src/com/BeginnerLevelFundamentals/AcademyOceanAssignment/App2.java
@@ -1,25 +1,43 @@
 package com.BeginnerLevelFundamentals.AcademyOceanAssignment;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter how many numbers to display: ");
+
+        try {
+            int numbers = scanner.nextInt();
+            fibonacciSeries(numbers);
+        } catch (InputMismatchException e) {
+            System.out.println("You didn't enter a number");
+        }
+    }
+
+    // Bogdan suggested to put my code in a separate method, have a validation for 0 entered numbers
+    // and catch an exception for String input
+
+    public static void fibonacciSeries(int numbers) {
 
         int number1 = 0;
         int number2 = 1;
         int number3;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter how many numbers to display: ");
-        int numbers = scanner.nextInt();
 
-        System.out.print(number1 + " " + number2);
-        for (int i = 2; i < numbers; i++) {
-            number3 = number1 + number2;
-            System.out.print(" " + number3);
-            number1 = number2;
-            number2 = number3;
+        if (numbers == 0) {
+            System.out.println("Please enter a higher number");
+        } else {
+            System.out.print(number1 + " " + number2);
+            for (int i = 2; i < numbers; i++) {
+                number3 = number1 + number2;
+                System.out.print(" " + number3);
+                number1 = number2;
+                number2 = number3;
+            }
         }
     }
 }
