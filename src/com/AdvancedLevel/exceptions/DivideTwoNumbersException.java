d8db7864975abc20c454510c3fab0f6f0eb15c43
Daniel Baykov <danibaikov@gmail.com>
2023-04-23 15:45:30 +0300

    adding Exceptions - Unchecked exception

--- src/com/AdvancedLevel/exceptions/DivideTwoNumbersException.java
+++ src/com/AdvancedLevel/exceptions/DivideTwoNumbersException.java
@@ -4,5 +4,23 @@ public class DivideTwoNumbersException {
 
     public static void main(String[] args) {
 
+        System.out.println(divide(30, 2));
+        System.out.println(divide(15, 0));
+        System.out.println("The job is finished");
+    }
+
+    private static int divide(int dividend, int divisor) {
+        try {
+            return dividend / divisor;
+        } catch (ArithmeticException e) {
+            System.err.println("The divisor can't be zero");
+            e.printStackTrace();
+            return 0;
+        } catch (NullPointerException e) {
+            System.err.println("NullPointerException is thrown");
+            return 0;
+        } finally {
+            System.out.println("Hey, I'm a very good tool to close files and database connections");
+        }
     }
 }
