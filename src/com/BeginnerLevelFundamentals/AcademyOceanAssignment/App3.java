a317ca7093a00ea653c0acd69b34bd6dbd8921ea
Daniel Baykov <danibaikov@gmail.com>
2023-04-24 07:52:02 +0300

    adding Third exercise after refactoring the code

--- src/com/BeginnerLevelFundamentals/AcademyOceanAssignment/App3.java
+++ src/com/BeginnerLevelFundamentals/AcademyOceanAssignment/App3.java
@@ -1,5 +1,6 @@
 package com.BeginnerLevelFundamentals.AcademyOceanAssignment;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App3 {
@@ -7,19 +8,36 @@ public class App3 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose a number: ");
-        int n = scanner.nextInt();
 
+        int n = 0;
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("Choose a number from 1 to 9: ");
+            try {
+                n = scanner.nextInt();
+                if (n >= 1 && n <= 10) {
+                    valid = true;
+                } else {
+                    System.out.println("Number out of range");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter a number from 1 to 9");
+                scanner.nextLine();
+            }
+        }
+
+        printTriangles(n);
+    }
+
+    private static void printTriangles(int n) {
         // nested loops for the first triangle
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
 
-            // 1 space for the first row
-            System.out.print(" ");
-
-            // 4 spaces between the two triangles
+            // spaces between the two triangles
             int l = n;
             while (i < l) {
                 System.out.print("    ");
@@ -29,9 +47,10 @@ public class App3 {
             // nested loops for the second triangle
             int k = n;
             for (int j = k + 1 - i; j <= n; j++) {
-                System.out.print(j + " ");
+                System.out.print(" " + j);
                 k--;
             }
+
             // empty line between rows
             System.out.println();
         }
