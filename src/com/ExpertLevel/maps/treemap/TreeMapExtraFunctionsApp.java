38e7015a0848f438e8b7782c2441626b737fced7
Daniel Baykov <danibaikov@gmail.com>
2023-04-25 10:19:45 +0300

    adding Advanced Level - TreeMap - extra functions 2/4

--- src/com/ExpertLevel/maps/treemap/TreeMapExtraFunctionsApp.java
+++ src/com/ExpertLevel/maps/treemap/TreeMapExtraFunctionsApp.java
@@ -1,5 +1,6 @@
 package com.ExpertLevel.maps.treemap;
 
+import java.util.NavigableMap;
 import java.util.TreeMap;
 
 public class TreeMapExtraFunctionsApp {
@@ -15,8 +16,22 @@ public class TreeMapExtraFunctionsApp {
         productPricesTreeMap.put("Teabox", 3.2);
 
         System.out.println("The first product in the map: " + productPricesTreeMap.firstKey());
+        System.out.println("TreeMap:");
         for (String product : productPricesTreeMap.keySet()) {
             System.out.println(product + " $" + productPricesTreeMap.get(product));
         }
+
+        System.out.println();
+        System.out.println("DescendingMap:");
+        NavigableMap<String, Double> productPriceDescendingMap = productPricesTreeMap.descendingMap();
+        for (String product : productPriceDescendingMap.keySet()) {
+            System.out.println(product + " $" + productPriceDescendingMap.get(product));
+        }
+
+        System.out.println();
+        System.out.println("Lower key: " + productPricesTreeMap.lowerKey("Cereals"));
+        System.out.println("Floor key: " + productPricesTreeMap.floorKey("Cereals"));
+        System.out.println("Higher key: " + productPricesTreeMap.higherKey("Cereals"));
+        System.out.println("Ceiling key: " + productPricesTreeMap.ceilingKey("Cereals"));
     }
 }
