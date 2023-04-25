22e41724027994458f4e8e78d55d0862f591989c
Daniel Baykov <danibaikov@gmail.com>
2023-04-25 08:30:58 +0300

    adding Advanced Level - HashMap - contains, getOrDefault and replace functions

--- src/com/ExpertLevel/maps/hashmap/HashMapApp.java
+++ src/com/ExpertLevel/maps/hashmap/HashMapApp.java
@@ -17,5 +17,22 @@ public class HashMapApp {
         capitals.remove("England", "Oslo");
         System.out.println("The capital of England is: " + capitals.get("England"));
 
+        if (capitals.containsValue("Moscow")) {
+            System.out.println("Russia is on the list");
+        }
+
+        capitals.put(null, "The city is not on the list");
+        String userInput = "Denmark";
+        if (capitals.containsKey(userInput)) {
+            System.out.println(capitals.get(userInput));
+        } else {
+            System.out.println(capitals.get(null));
+        }
+
+        System.out.println(capitals.getOrDefault(userInput, "The city is not on the list"));
+
+        capitals.replace("India", "Ankara", "Mumbai");
+        System.out.println("The capital of India is: " + capitals.get("India"));
+
     }
 }
