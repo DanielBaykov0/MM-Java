fc590a07c41f48eaf59d5d821887901ccb3d2b43
Daniel Baykov <danibaikov@gmail.com>
2023-04-27 16:41:55 +0300

    Academy Ocean OOP Challenge - commit after changes

--- src/com/IntermediateLevelOOP/AcademyOceanAssignment/Assassin.java
+++ src/com/IntermediateLevelOOP/AcademyOceanAssignment/Assassin.java
@@ -1,4 +1,36 @@
 package com.IntermediateLevelOOP.AcademyOceanAssignment;
 
-public class Assassin {
+public class Assassin extends Hero {
+
+
+    public Assassin() {
+        super("Assassin");
+    }
+
+    @Override
+    public String getQuote() {
+        return "One kills a man, one is an assassin; one kills millions,\n" +
+                "one is a conqueror; one kills everybody, one is a god.";
+    }
+
+    @Override
+    public int attack() {
+        int attackNumber = super.attack();
+        int specialAttackNumber = RANDOM_NUMBER_GENERATOR.nextInt(1, 101);
+        if (isSpecialAttack(specialAttackNumber)) {
+            return attackNumber * 3;
+        } else {
+            return attackNumber;
+        }
+    }
+
+    @Override
+    public int defend() {
+        return super.defend();
+    }
+
+    private boolean isSpecialAttack(int specialAttackNumber) {
+        int specialAttackPercent = 30;
+        return specialAttackNumber <= specialAttackPercent;
+    }
 }
--- src/com/IntermediateLevelOOP/AcademyOceanAssignment/Hero.java
+++ src/com/IntermediateLevelOOP/AcademyOceanAssignment/Hero.java
@@ -1,4 +1,39 @@
 package com.IntermediateLevelOOP.AcademyOceanAssignment;
 
-public class Hero {
+import java.util.Random;
+
+public abstract class Hero {
+
+    protected final Random RANDOM_NUMBER_GENERATOR = new Random();
+    private final String className;
+    protected int HEALTH_POINTS = 100;
+    protected int ATTACK_POINTS = 20;
+    protected int ARMOR_POINTS = 20;
+
+    public Hero(String className) {
+        this.className = className;
+    }
+
+    public abstract String getQuote();
+
+    public int attack() {
+        return RANDOM_NUMBER_GENERATOR.nextInt((int) (ATTACK_POINTS * 0.8), (int) (ATTACK_POINTS * 1.21));
+    }
+    public int defend() {
+        return RANDOM_NUMBER_GENERATOR.nextInt((int) (ATTACK_POINTS * 0.8), (int) (ATTACK_POINTS * 1.21));
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero: " +
+                "'" + className + '\'' +
+                ", healthPoints=" + HEALTH_POINTS +
+                ", attackPoints=" + ATTACK_POINTS +
+                ", armorPoints=" + ARMOR_POINTS;
+    }
+
 }
