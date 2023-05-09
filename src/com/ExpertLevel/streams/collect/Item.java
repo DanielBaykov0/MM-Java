4925f5637136c0433d1455380fc045130911cfd2
Daniel Baykov <danibaikov@gmail.com>
2023-05-09 13:36:26 +0300

    adding Functional Programming - Streams - peek()

--- src/com/ExpertLevel/streams/collect/Item.java
+++ src/com/ExpertLevel/streams/collect/Item.java
@@ -2,9 +2,9 @@ package com.ExpertLevel.streams.collect;
 
 public class Item {
 
-    private final String name;
-    private final int quantity;
-    private final double price;
+    private String name;
+    private int quantity;
+    private double price;
 
     public Item(String name, int quantity, double price) {
         this.name = name;
@@ -22,6 +22,18 @@ public class Item {
 
     public double getPrice() {
         return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     @Override
