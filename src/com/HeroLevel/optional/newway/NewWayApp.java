9925f550b6204cf6ed299e246fee1b4e4733bd3b
Daniel Baykov <danibaikov@gmail.com>
2023-05-14 11:45:38 +0300

    adding Hero Level - Optional - the new way (null check)

---
+++ src/com/HeroLevel/optional/newway/NewWayApp.java
@@ -0,0 +1,21 @@
+package com.HeroLevel.optional.newway;
+
+import com.HeroLevel.optional.oldway.Friend;
+import com.HeroLevel.optional.oldway.FriendFinderService;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class NewWayApp {
+
+    public static void main(String[] args) {
+
+        FriendFinderService friendFinderService = new FriendFinderService();
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Find friend:");
+        Optional<Friend> friend = Optional.ofNullable(friendFinderService.findFriend(userInput.nextLine()));
+        friend.ifPresentOrElse(System.out::println, () -> System.out.println("Not found"));
+
+    }
+}
