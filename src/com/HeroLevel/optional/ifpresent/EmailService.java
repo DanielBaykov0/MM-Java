74f683e55f03ce20400f7b5ba0180ba6cf1ba984
Daniel Baykov <danibaikov@gmail.com>
2023-05-14 15:45:33 +0300

    adding Hero Level - Optional - ifPresent()

---
+++ src/com/HeroLevel/optional/ifpresent/EmailService.java
@@ -0,0 +1,10 @@
+package com.HeroLevel.optional.ifpresent;
+
+public class EmailService {
+
+    public static void sendPromotion(User user) {
+        user.getEmail().ifPresent(
+                email -> System.out.println("Promotion email is sent to: " + email)
+        );
+    }
+}
