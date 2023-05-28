ca1a605185aa2424ad22aa73677c52709ef66e50
Daniel Baykov <danibaikov@gmail.com>
2023-05-28 18:15:19 +0300

    adding bare-bones test classes for Hangman game

---
+++ src/test/java/com/AdvancedLevel/FinalPractice/FileClassTest.java
@@ -0,0 +1,32 @@
+package com.AdvancedLevel.FinalPractice;
+
+import org.junit.jupiter.api.Test;
+
+class FileClassTest {
+
+    @Test
+    void testConstructor() {
+
+    }
+
+    @Test
+    void testListAllFiles() {
+
+    }
+
+    @Test
+    void testChooseFile() {
+
+    }
+
+    @Test
+    void testCreateNewFile() {
+
+    }
+
+    @Test
+    void testWriteWordsToFile() {
+
+    }
+}
+
--- src/test/java/com/AdvancedLevel/FinalPractice/HangmanServiceTest.java
+++ src/test/java/com/AdvancedLevel/FinalPractice/HangmanServiceTest.java
@@ -1,24 +1,12 @@
 package com.AdvancedLevel.FinalPractice;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class HangmanServiceTest {
-    /**
-     * Method under test: {@link HangmanService#startGame()}
-     */
+
     @Test
-    @Disabled("TODO: Complete this test")
     void testStartGame() {
-        // TODO: Complete this test.
-        //   Diffblue AI was unable to find a test
 
-        // Arrange and Act
-        // TODO: Populate arranged inputs
-//        HangmanService.startGame();
-
-        // Assert
-        // TODO: Add assertions on result
     }
 }
 
